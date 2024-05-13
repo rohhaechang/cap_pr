8ed96002dd4a4987fd6dffd80d971b6878d45fa9
haechang <lunarbalance1@gmail.com>
2024-05-13 22:09:11 +0900

    final real

--- app/item/[company_cik]/page.tsx
+++ app/item/[company_cik]/page.tsx
@@ -1,3 +1,36 @@
 import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
+
+const fetchItemByCik = async (company_cik: string) => {
+  const item = await prisma.table1.findFirst({
+    where: {
+      company_cik: parseInt(company_cik)
+    },
+    select: {
+      id: true,
+      company_cik: true,
+      year: true,
+      product_service_new: true,
+      revenue_productnregion: true,
+      netsales_productnregion: true,
+    }
+  })
+  if (!item) {
+    throw new Error();
+  }
+  return item;
+}
+
+export default async function ItemDetails({params}: {params: {company_cik: number}}) {
+  const items = await fetchItemByCik(params.company_cik.toString());
+  return (
+    <main>
+      <div>
+        <div>{items.company_cik}</div>
+        <div>{items.product_service_new}</div>
+        <div>{items.netsales_productnregion}</div>
+      `</div>
+    </main>
+  )
+}
