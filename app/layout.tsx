ca5db21c68e2339f3573de45ca721ed041b1368f
haechang <94176916+rohhaechang@users.noreply.github.com>
2024-05-13 20:40:27 +0900

    third

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
-import { NavLinks } from '@/app/ui/nav-links'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <NavLinks />
         <main>{children}</main>
       </body>
     </html>
