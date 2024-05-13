8cfead4ecd057c5b91ac37d47a65e16fabf49072
haechang <lunarbalance1@gmail.com>
2024-05-13 20:50:55 +0900

    final

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,11 @@
 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
