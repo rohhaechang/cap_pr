a52e714317afe820328c0e5a1b6a4486fef924d2
haechang <lunarbalance1@gmail.com>
2024-05-15 17:03:39 +0900

    asdlfkjasdf

--- app/item/[company_cik]/page.tsx
+++ app/item/[company_cik]/page.tsx
@@ -3,6 +3,10 @@ import { randomInt } from "crypto";
 
 const prisma = new PrismaClient();
 
+interface Topic {
+  [key: string]: string;
+}
+
 const fetchItemByCik = async (company_cik: string) => {
   const item = await prisma.table1.findFirst({
     where: {
@@ -96,7 +100,7 @@ export default async function ItemDetails({params}: {params: {company_cik: numbe
         <h2>item_3</h2>
           <div>
         {item_3['topics'].length > 1
-          ? item_3['topics'].map((topic: any) => <p key={randomInt(300)}>{Object.keys(topic)} {topic.Object.keys(topic)}</p>)
+          ? item_3['topics'].map((topic: Topic) => <p key={randomInt(300)}>{Object.keys(topic)}{topic[Object.keys(topic)]}</p>)
           : <p>topics: none</p>}
           </div>
         <h2>item_5</h2>
