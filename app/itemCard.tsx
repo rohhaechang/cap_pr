85e9b8eb3f120989ae4cfa6dceeef112b5ba4da4
haechang <lunarbalance1@gmail.com>
2024-05-15 21:02:58 +0900

    flkjef

--- app/itemCard.tsx
+++ app/itemCard.tsx
@@ -7,9 +7,9 @@ interface Props {
 
 export default function ItemCard({ item }: Props) {
   return (
-    <div style={{height: "20%"}}>
+    <div style={{backgroundColor: "lightskyblue", border: "1px solid gray"}}>
       <Link href={`/item/${item.company_cik}`}>
-        <div key={item.id}><text style={{fontSize: "20px"}}>{item.name}</text>: { item.company_cik}</div>
+        <div key={item.id}><text style={{fontSize: "20px"}}>{item.name}</text> - { item.company_cik}</div>
       </Link>
     </div>
   )
